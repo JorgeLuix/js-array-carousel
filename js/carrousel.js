ec3ad1c98bc24cdf9f6f089c8e79132899a21d1a
JorgeLuix <tec.jorge.29@gmai.com>
2023-03-14 17:50:04 +0100

    strutturando html slides

---
+++ js/carrousel.js
@@ -0,0 +1,14 @@
+/*template slide*/
+
+`   <div class="slide active w-100 p-0 ">
+        <img class="img-fluid d-block object-fit-cover" 
+        src="./img/01.webp" alt="">
+    </div>`
+/******template slide********/
+
+/*template thmub*/
+ `  <div class="thumb w-100 p-0">
+        <img class="img-fluid d-block object-fit-cover"
+         src="./img/01.webp" alt="">
+    </div>`
+/*****template thmub****/
