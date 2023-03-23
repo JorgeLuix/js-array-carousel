bdaa966be6d6048ae3697f7390de407a7c3b02dc
JorgeLuix <tec.jorge.29@gmai.com>
2023-03-23 17:59:31 +0100

    aggiunta foreach fx autoplay hover

--- js/carrousel.js
+++ js/carrousel.js
@@ -13,45 +13,85 @@
 
 
 const images = [
-    'img/01.webp',
-    'img/02.webp',
-    'img/03.webp',
-    'img/04.webp',
-    'img/05.webp'
+
+    {   img: 'img/01.webp',
+        title: 'Spider-Man'
+
+    },
+    {   img: 'img/02.webp',
+        title: 'Lupi'
+
+    },
+    {   img: 'img/03.webp',
+        title: 'Amici'
+    },
+    {   img: 'img/04.webp',
+        title: 'Gato con lo zaino'
+
+    },
+    {   img: 'img/05.webp',
+        title: 'Avengers'
+
+    },
+   
 ];
 const slider = document.querySelector('.slider');
 const thumbnails = document.querySelector('.thumbnails-container');
 
 let currentIndex = 0;
 let slides = '';
-let thumbs = '';
-for (let i = 0; i < images.length; i++) {
+let thumbs = ''; 
 
+ images.forEach((contenuto) => {
     slides +=  `<div class="slide ">
-    <img src="${images[i]}" alt="spider-${i}" >
+    <img src="${contenuto.img}" alt="" >
+    <div class="opaci" > <h3>${contenuto.title}</h3> </div>
 </div>
 `;
     thumbs+= `<div class="thumb">
-    <img src="${images[i]} " alt="spider-${i} ">
+    <img src="${contenuto.img} " alt="">
+</div>`;
+ })
+/*for (let i = 0; i < images.length; i++) {
+
+    slides +=  `<div class="slide ">
+    <img src="${images[i].nome}" alt="${images[i].title}" >
+    <div class="opaci" > <h3>${images[i].title}</h3> </div>
+</div>
+`;
+    thumbs+= `<div class="thumb">
+    <img src="${images[i].nome} " alt="${images[i].title} ">
 </div>`;
 
-}
+}*/
 slider.innerHTML += slides;
 thumbnails.innerHTML += thumbs;
-document.querySelectorAll('.slide')[0].classList.add('active');
-document.querySelectorAll('.thumb')[0].classList.add('active');
-
-
+document.querySelectorAll('.slide')[currentIndex].classList.add('.active');
+document.querySelectorAll('.thumb')[currentIndex].classList.add('.active');
 
 const next = document.querySelector('.next');
 const prev = document.querySelector('.prev');
 
 next.addEventListener('click', goNext);
+prev.addEventListener('click', goPrev);
+
+//aggiungo evento hover
+slider.addEventListener('mouseenter', stopAutoplay);
+slider.addEventListener('mouseleave', startAutoplay);
+
+//funzione inizio automatico 
+function startAutoplay() {
+    intervalId = setInterval(goNext, 1000);
+  }
+  //funzione stop slider
+  function stopAutoplay() {
+    clearInterval(intervalId);
+  }
 
 function goNext() {
     document.querySelectorAll('.slide')[currentIndex].classList.remove('active');
     document.querySelectorAll('.thumb')[currentIndex].classList.remove('active');
-    if (currentIndex === images.lenght -1){
+    if (currentIndex === images.length -1){
         currentIndex = 0;
     } else {
         currentIndex++;
@@ -66,12 +106,12 @@ prev.addEventListener('click', goPrev);
         document.querySelectorAll('.slide')[currentIndex].classList.remove('active');
         document.querySelectorAll('.thumb')[currentIndex].classList.remove('active');
         if (currentIndex === 0){
-            currentIndex === images.lenght -1;
+            currentIndex === images.length -1;
         } else {
             currentIndex--;
         };
     document.querySelectorAll('.slide')[currentIndex].classList.add('active');
     document.querySelectorAll('.thumb')[currentIndex].classList.add('active');  
  };
-
+startAutoplay();
 
